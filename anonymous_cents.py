0d4bbe71880a4dad4717770107b0f29886b0cf0e
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2020-09-26 10:01:07 -0300

    Adiciona envio de emails

--- anonymous_cents.py
+++ anonymous_cents.py
@@ -7,11 +7,16 @@ with open('emails.py', 'r') as e_mails:
     emails_passados = e_mails.read()
     exec(emails_passados)
 
-def main(emails):
-    planilha_centavos, pagina_planilha_centavos = cria_planilha_emails('centavos')
-    planilha_apelidos, pagina_planilha_apelidos = cria_planilha_emails('apelidos')
 
+def main(emails):
+    planilha_centavos, pagina_planilha_centavos = cria_planilha_emails(
+        'centavos')
+    planilha_apelidos, pagina_planilha_apelidos = cria_planilha_emails(
+        'apelidos')
+
+    email_apelido = {}
     random.seed(1098)
+    
     raiz_apelido = random.randint(200, 300)
     for indice, email in enumerate(emails):
 
@@ -25,9 +30,13 @@ def main(emails):
         pagina_planilha_centavos.write(linha_email, email)
 
         pagina_planilha_apelidos.write(linha_apelido, apelido)
+        email_apelido[email] = apelido
 
     planilha_centavos.close()
     planilha_apelidos.close()
+
+    print("Os emails estão sendo enviados para: {}\n" .format(email_apelido))
+    return email_apelido
 
 
 def cria_planilha_emails(nome):
@@ -35,41 +44,40 @@ def cria_planilha_emails(nome):
     pagina_planilha = planilha.add_worksheet()
 
     bold = planilha.add_format({'bold': True})
-    pagina_planilha.set_column('A:A', 50, bold) 
+    pagina_planilha.set_column('A:A', 50, bold)
 
     pagina_planilha.write('A1', 'E-mails')
 
     return (planilha, pagina_planilha)
 
 
-if __name__ == '__main__':
-    main(emails)
+def envia_email(retorno):
 
+    for dest, apelido in retorno.items():
 
-def envia_email(dicionario):
+        remetente = 'leandra.silva@ccc.ufcg.edu.br'
+        senha = 'leandrinha1717'
 
-    remetente = 'leandra.silva@ccc.ufcg.edu.br'
-    senha = 'leandrinha1717'
+        destinatario = dest
+        mensagem = "Oi, esse eh o seu apelido na planilha centavos.xlsx : " + str(apelido)
+        email_text = """\
 
-    destinatario = 'rodrigo.cavalcanti@ccc.ufcg.edu.br'
-    subject = 'Apelido centavos LOAC'
-    mensagem = "Seu apelido na planilha centavos.xlsx : "
+        From: %s
+        To: %s
 
-    email_text = """\
-    remetente: %s
-    destinatario: %s
-    subject: %s
-
-    %s
-    """ % (remetente, ", ".join(destinatario), subject, mensagem)
+        %s
+        """ % (remetente, destinatario, mensagem)
 
     try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(remetente, senha)
-    server.sendmail(remetente, destinatario, email_text)
-    server.close()
-
-    print ('Email enviado com sucesso!')
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(remetente, senha)
+        server.sendmail(remetente,destinatario ,'Subject: Apelido planilha de centavos LOAC\n{}'.format(email_text))
+        server.close()
+        print('Email enviado com sucesso!')
     except:
-    print ('O email não foi enviado')
+        print('O email não foi enviado')
+
+if __name__ == '__main__':
+    retorno = main(emails)
+    envia_email(retorno)
