7ceb716f94f0f35fe789f16cda49567a6e8def35
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-10-12 19:28:29 -0300

    BugFix: Ao iniciar o sistema os tokens ficavam com o valor None, o que causava o projeto a quebrar

--- src/scripts/util/manipula_planilhas.py
+++ src/scripts/util/manipula_planilhas.py
@@ -7,9 +7,6 @@ from src.scripts.util.envia_email import envia_email
 from src.scripts.util.verificadores import verifica_existencia_emails_cadastrados
 from src.scripts.util.dicionario_apelidos import cria_dicionario_apelidos
 
-TOKEN_APELIDOS = str(config('TOKEN_APELIDOS', default=None))
-TOKEN_CENTAVOS = str(config('TOKEN_CENTAVOS', default=None))
-
 def inicia_servidor_planilhas(identificador_planilha):
 
     server_gc = gspread.service_account(filename='credentials.json')
@@ -31,9 +28,9 @@ def cria_planilhas():
     lista_emails = [linha.rstrip(' \n') for linha in linhas if linha.rstrip(' \n') != '']
     dict_apelidos = cria_dicionario_apelidos(lista_emails)
 
-    planilha_centavos, pagina_centavos = inicia_servidor_planilhas(TOKEN_CENTAVOS)
+    planilha_centavos, pagina_centavos = inicia_servidor_planilhas(str(config('TOKEN_CENTAVOS')))
     pagina_centavos.clear()
-    _, pagina_emails_apelidos = inicia_servidor_planilhas(TOKEN_APELIDOS)
+    _, pagina_emails_apelidos = inicia_servidor_planilhas(str(config('TOKEN_APELIDOS')))
     pagina_emails_apelidos.clear()
 
     for email, apelido in dict_apelidos.items():
@@ -48,8 +45,8 @@ def cria_planilhas():
     envia_email()
 
 def add_email_unico(email, apelido):
-    _, pagina_centavos = inicia_servidor_planilhas(TOKEN_CENTAVOS)
-    _, pagina_emails_apelidos = inicia_servidor_planilhas(TOKEN_APELIDOS)
+    _, pagina_centavos = inicia_servidor_planilhas(str(config('TOKEN_CENTAVOS')))
+    _, pagina_emails_apelidos = inicia_servidor_planilhas(str(config('TOKEN_APELIDOS')))
 
     add_planilha([apelido], pagina_centavos)
     add_planilha([email, apelido], pagina_emails_apelidos)
